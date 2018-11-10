c8e831b3c160d10ceb4dc5dc5f759be4b5eb937f
benghui <benghui@gmail.com>
2018-11-10 16:05:06 +0800

    added button click handler

--- script.js
+++ script.js
@@ -1,27 +1,36 @@
 class List extends React.Component {
-  constructor(){
-    super()
-    this.changeHandler = this.changeHandler.bind( this );
-  }
+
+  changeHandler = this.changeHandler.bind( this );
+  addToList = this.addToList.bind(this)
 
   state = {
-    list : [],
-    word : ""
+    list: [],
+    word: ""
   }
 
   changeHandler(event){
-    this.setState({word:event.target.value});
-    console.log("change", event.target.value);
+    this.setState({word: event.target.value});
+  }
+
+  addToList() {
+    let listItem = this.state.list
+    listItem.unshift(this.state.word)
+    this.setState({ list: listItem, word: ""})
   }
 
   render() {
       // render the list with a map() here
+      let list = this.state.list.map ((list, index) => {
+        return <li key={index}>{list}</li>
+      })
 
-      console.log("rendering");
       return (
         <div className="list">
           <input onChange={this.changeHandler} value={this.state.word}/>
-          <button>add item</button>
+          <button onClick = {this.addToList}>add item</button>
+          <ul>
+            {list}
+          </ul>
         </div>
       );
   }
@@ -31,4 +40,3 @@ ReactDOM.render(
     <List/>,
     document.getElementById('root')
 );
-
