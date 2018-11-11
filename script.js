fc3a2da6d8ecde3937cf7daa1c6d3d997d8c0514
benghui <benghui@gmail.com>
2018-11-11 15:58:31 +0800

    added length validation for input

--- script.js
+++ script.js
@@ -1,38 +1,40 @@
 class List extends React.Component {
-
-  changeHandler = this.changeHandler.bind( this );
-  addToList = this.addToList.bind(this)
+  changeHandler = this.changeHandler.bind(this);
+  addToList = this.addToList.bind(this);
 
   state = {
     list: [],
-    word: ""
-  }
+    word: "",
+    error: "Please enter an item"
+  };
 
-  changeHandler(event){
-    this.setState({word: event.target.value});
+  changeHandler(event) {
+    if (event.target.value.length > 0) {
+      this.setState({ error: "" });
+    }
+    this.setState({ word: event.target.value });
   }
 
   addToList() {
-    let listItem = this.state.list
-    listItem.unshift(this.state.word)
-    this.setState({ list: listItem, word: ""})
+    let listItem = this.state.list;
+    listItem.unshift(this.state.word);
+    this.setState({ list: listItem, word: "", error: "Please enter an item" });
   }
 
   render() {
-      // render the list with a map() here
-      let list = this.state.list.map ((list, index) => {
-        return <li key={index}>{list}</li>
-      })
+    // render the list with a map() here
+    let list = this.state.list.map((list, index) => {
+      return <li key={index}>{list}</li>;
+    });
 
-      return (
-        <div className="list">
-          <input onChange={this.changeHandler} value={this.state.word}/>
-          <button onClick = {this.addToList}>add item</button>
-          <ul>
-            {list}
-          </ul>
-        </div>
-      );
+    return (
+      <div className="list">
+        <input onChange={this.changeHandler} value={this.state.word} />
+        <button onClick={this.addToList}>add item</button>
+        {this.state.error}
+        <ul>{list}</ul>
+      </div>
+    );
   }
 }
 
